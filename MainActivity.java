d87717851c9827c5faeca5856be4f0182ae19943
Marinelan111 <aagjno5588@gmail.com>
2025-11-06 19:05:25 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lab3;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,9 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int SCISSORS = 0;
+    private static final int STONE = 1;
+    private static final int PAPER = 2;
 
     private EditText ed_name;
     private TextView tv_text, tv_name, tv_winner, tv_mmora, tv_cmora;
@@ -21,60 +25,80 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ed_name = findViewById(R.id.ed_name);
+        initializeViews();
 
+        btn_mora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playGame();
+            }
+        });
+    }
+    private void initializeViews() {
+        ed_name = findViewById(R.id.ed_name);
         tv_text = findViewById(R.id.tv_text);
         tv_name = findViewById(R.id.tv_name);
         tv_winner = findViewById(R.id.tv_winner);
         tv_mmora = findViewById(R.id.tv_mmora);
         tv_cmora = findViewById(R.id.tv_cmora);
-
         btn_scissor = findViewById(R.id.btn_scissor);
         btn_stone = findViewById(R.id.btn_stone);
         btn_paper = findViewById(R.id.btn_paper);
-
         btn_mora = findViewById(R.id.btn_mora);
+    }
+    private void playGame() {
+        String playerName = ed_name.getText().toString();
 
-        btn_mora.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (ed_name.length() < 1) {
-                    tv_text.setText("請輸入玩家姓名");
-                } else {
-                    tv_name.setText(String.format("名字\n%s", ed_name.getText().toString()));
+        if (TextUtils.isEmpty(playerName)) {
+            tv_text.setText("請輸入玩家姓名");
+            return;
+        }
 
-                    if (btn_scissor.isChecked()) {
-                        tv_mmora.setText("我方出拳\n剪刀");
-                    } else if (btn_stone.isChecked()) {
-                        tv_mmora.setText("我方出拳\n石頭");
-                    } else {
-                        tv_mmora.setText("我方出拳\n布");
-                    }
-                    int computer_random = (int) (Math.random() * 3);
-                    if (computer_random == 0) {
-                        tv_cmora.setText("電腦出拳\n剪刀");
-                    } else if (computer_random == 1) {
-                        tv_cmora.setText("電腦出拳\n石頭");
-                    } else {
-                        tv_cmora.setText("電腦出拳\n布");
-                    }
+        int playerMove = getPlayerMove();
+        int computerMove = (int) (Math.random() * 3);
 
-                    if ((btn_scissor.isChecked() && computer_random == 2) ||
-                            (btn_stone.isChecked() && computer_random == 0) ||
-                            (btn_paper.isChecked() && computer_random == 1)) {
-                        tv_winner.setText("勝利者\n" + ed_name.getText().toString());
-                        tv_text.setText("恭喜您獲勝了！！！");
-                    } else if ((btn_scissor.isChecked() && computer_random == 1) ||
-                            (btn_stone.isChecked() && computer_random == 2) ||
-                            (btn_paper.isChecked() && computer_random == 0)) {
-                        tv_winner.setText("勝利者\n電腦");
-                        tv_text.setText("可惜，電腦獲勝了！");
-                    } else {
-                        tv_winner.setText("勝利者\n平手");
-                        tv_text.setText("平局，請再試一次！");
-                    }
-                }
-            }
-        });
+        updateMovesUI(playerName, playerMove, computerMove);
+
+        determineWinner(playerName, playerMove, computerMove);
+    }
+    private int getPlayerMove() {
+        if (btn_scissor.isChecked()) {
+            return SCISSORS;
+        } else if (btn_stone.isChecked()) {
+            return STONE;
+        } else {
+            return PAPER;
+        }
+    }
+    private String getMoveString(int move) {
+        switch (move) {
+            case SCISSORS:
+                return "剪刀";
+            case STONE:
+                return "石頭";
+            case PAPER:
+                return "布";
+            default:
+                return "";
+        }
+    }
+    private void updateMovesUI(String playerName, int playerMove, int computerMove) {
+        tv_name.setText(String.format("名字\n%s", playerName));
+        tv_mmora.setText(String.format("我方出拳\n%s", getMoveString(playerMove)));
+        tv_cmora.setText(String.format("電腦出拳\n%s", getMoveString(computerMove)));
+    }
+    private void determineWinner(String playerName, int playerMove, int computerMove) {
+        if (playerMove == computerMove) {
+            tv_winner.setText("勝利者\n平手");
+            tv_text.setText("平局，請再試一次！");
+        } else if ((playerMove == PAPER && computerMove == STONE) ||
+                (playerMove == STONE && computerMove == SCISSORS) ||
+                (playerMove == SCISSORS && computerMove == PAPER)) {
+            tv_winner.setText("勝利者\n" + playerName);
+            tv_text.setText("恭喜您獲勝了！！！");
+        } else {
+            tv_winner.setText("勝利者\n電腦");
+            tv_text.setText("可惜，電腦獲勝了！");
+        }
     }
 }
